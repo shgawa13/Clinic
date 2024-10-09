c66c2775178619be9cff7ecb0baa60f86e197912
shgawa <shgawa.cp@gmail.com>
2024-10-10 00:45:12 +0300

    adding clsCountries

---
+++ Business/Business/clsCountries.cs
@@ -0,0 +1,64 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+  public class clsCountries
+  {
+    public class clsCounrty
+    {
+      public int ID { set; get; }
+      public string CountryName { set; get; }
+
+      public clsCounrty()
+      {
+        this.ID = -1;
+        this.CountryName = "";
+      }
+
+      private clsCounrty(int ID, string CountryName)
+      {
+        this.ID = ID;
+        this.CountryName = CountryName;
+      }
+
+      public static clsCounrty Find(int ID)
+      {
+        string CountryName = "";
+        if (clsCountriesData.GetCountryByID(ID, ref CountryName))
+        {
+          return new clsCounrty(ID, CountryName);
+        }
+        else
+        {
+
+          return null;
+        }
+
+      }
+
+      public static clsCounrty Find(string CountryName)
+      {
+        int ID = -1;
+        if (clsCountriesData.GetCountryByName(CountryName, ref ID))
+        {
+          return new clsCounrty(ID, CountryName);
+        }
+        else
+        {
+          return null;
+        }
+
+      }
+
+      public static DataTable GetAllCountries() => clsCountriesData.GetAllCountries();
+
+
+    }
+  }
+}
