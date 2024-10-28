ec5f6ef4e19418cf531ec3a446151ded3104dbd9
shgawa <shgawa.cp@gmail.com>
2024-10-28 03:57:57 +0300

    adding Custom Button Class

--- Dental App/Dental App/Doctors/frmDoctors.Designer.cs
+++ Dental App/Dental App/Doctors/frmDoctors.Designer.cs
@@ -28,80 +28,171 @@
     /// </summary>
     private void InitializeComponent()
     {
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.lblPatientsNumbers = new System.Windows.Forms.Label();
       this.label1 = new System.Windows.Forms.Label();
       this.tabControl1 = new System.Windows.Forms.TabControl();
-      this.tabPage1 = new System.Windows.Forms.TabPage();
+      this.tpListDoctors = new System.Windows.Forms.TabPage();
       this.tabPage2 = new System.Windows.Forms.TabPage();
+      this.panel3 = new System.Windows.Forms.Panel();
+      this.iconButton1 = new FontAwesome.Sharp.IconButton();
+      this.expertsBtn1 = new Dental_App.ExpertsBtn();
+      this.panel1.SuspendLayout();
       this.tabControl1.SuspendLayout();
+      this.panel3.SuspendLayout();
       this.SuspendLayout();
+      // 
+      // panel1
+      // 
+      this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(29)))), ((int)(((byte)(67)))));
+      this.panel1.Controls.Add(this.expertsBtn1);
+      this.panel1.Controls.Add(this.lblPatientsNumbers);
+      this.panel1.Controls.Add(this.label1);
+      this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.panel1.Location = new System.Drawing.Point(0, 474);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(800, 56);
+      this.panel1.TabIndex = 3;
+      // 
+      // lblPatientsNumbers
+      // 
+      this.lblPatientsNumbers.AutoSize = true;
+      this.lblPatientsNumbers.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblPatientsNumbers.ForeColor = System.Drawing.Color.WhiteSmoke;
+      this.lblPatientsNumbers.Location = new System.Drawing.Point(87, 16);
+      this.lblPatientsNumbers.Name = "lblPatientsNumbers";
+      this.lblPatientsNumbers.Size = new System.Drawing.Size(37, 21);
+      this.lblPatientsNumbers.TabIndex = 2;
+      this.lblPatientsNumbers.Text = "???";
       // 
       // label1
       // 
-      this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
       this.label1.AutoSize = true;
-      this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
       this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
-      this.label1.Location = new System.Drawing.Point(340, 10);
+      this.label1.Location = new System.Drawing.Point(3, 16);
       this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(89, 26);
-      this.label1.TabIndex = 0;
-      this.label1.Text = "Doctors";
+      this.label1.Size = new System.Drawing.Size(88, 21);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "# Record: ";
       // 
       // tabControl1
       // 
-      this.tabControl1.Controls.Add(this.tabPage1);
+      this.tabControl1.Controls.Add(this.tpListDoctors);
       this.tabControl1.Controls.Add(this.tabPage2);
+      this.tabControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
       this.tabControl1.Font = new System.Drawing.Font("Franklin Gothic Medium", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.tabControl1.Location = new System.Drawing.Point(12, 30);
+      this.tabControl1.Location = new System.Drawing.Point(0, 2);
       this.tabControl1.Name = "tabControl1";
       this.tabControl1.SelectedIndex = 0;
-      this.tabControl1.Size = new System.Drawing.Size(776, 424);
-      this.tabControl1.TabIndex = 1;
+      this.tabControl1.Size = new System.Drawing.Size(800, 472);
+      this.tabControl1.TabIndex = 4;
       // 
-      // tabPage1
+      // tpListDoctors
       // 
-      this.tabPage1.BackColor = System.Drawing.Color.Gainsboro;
-      this.tabPage1.Location = new System.Drawing.Point(4, 29);
-      this.tabPage1.Name = "tabPage1";
-      this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-      this.tabPage1.Size = new System.Drawing.Size(768, 391);
-      this.tabPage1.TabIndex = 0;
-      this.tabPage1.Text = "List Doctors";
+      this.tpListDoctors.BackColor = System.Drawing.Color.Gainsboro;
+      this.tpListDoctors.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.tpListDoctors.Location = new System.Drawing.Point(4, 29);
+      this.tpListDoctors.Name = "tpListDoctors";
+      this.tpListDoctors.Padding = new System.Windows.Forms.Padding(3);
+      this.tpListDoctors.Size = new System.Drawing.Size(792, 439);
+      this.tpListDoctors.TabIndex = 0;
+      this.tpListDoctors.Text = "List Doctors";
       // 
       // tabPage2
       // 
       this.tabPage2.BackColor = System.Drawing.Color.Gainsboro;
+      this.tabPage2.ForeColor = System.Drawing.SystemColors.ControlText;
       this.tabPage2.Location = new System.Drawing.Point(4, 29);
       this.tabPage2.Name = "tabPage2";
       this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-      this.tabPage2.Size = new System.Drawing.Size(768, 373);
+      this.tabPage2.Size = new System.Drawing.Size(792, 439);
       this.tabPage2.TabIndex = 1;
-      this.tabPage2.Text = "Add new";
+      this.tabPage2.Text = "Add Doctors";
+      // 
+      // panel3
+      // 
+      this.panel3.BackColor = System.Drawing.Color.White;
+      this.panel3.Controls.Add(this.iconButton1);
+      this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.panel3.Location = new System.Drawing.Point(0, -93);
+      this.panel3.Name = "panel3";
+      this.panel3.Size = new System.Drawing.Size(800, 95);
+      this.panel3.TabIndex = 6;
+      // 
+      // iconButton1
+      // 
+      this.iconButton1.Anchor = System.Windows.Forms.AnchorStyles.None;
+      this.iconButton1.FlatAppearance.BorderSize = 0;
+      this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.iconButton1.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.iconButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(29)))), ((int)(((byte)(67)))));
+      this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.UserMd;
+      this.iconButton1.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(29)))), ((int)(((byte)(67)))));
+      this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+      this.iconButton1.IconSize = 55;
+      this.iconButton1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+      this.iconButton1.Location = new System.Drawing.Point(324, 3);
+      this.iconButton1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 9);
+      this.iconButton1.Name = "iconButton1";
+      this.iconButton1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+      this.iconButton1.Size = new System.Drawing.Size(131, 86);
+      this.iconButton1.TabIndex = 1;
+      this.iconButton1.Text = "Doctors";
+      this.iconButton1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+      this.iconButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+      this.iconButton1.UseVisualStyleBackColor = true;
+      // 
+      // expertsBtn1
+      // 
+      this.expertsBtn1.BackColor = System.Drawing.Color.MediumSlateBlue;
+      this.expertsBtn1.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+      this.expertsBtn1.BorderColor = System.Drawing.Color.PaleVioletRed;
+      this.expertsBtn1.BorderRadius = 20;
+      this.expertsBtn1.BorderSize = 0;
+      this.expertsBtn1.FlatAppearance.BorderSize = 0;
+      this.expertsBtn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.expertsBtn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.expertsBtn1.ForeColor = System.Drawing.Color.Black;
+      this.expertsBtn1.Location = new System.Drawing.Point(275, 6);
+      this.expertsBtn1.Name = "expertsBtn1";
+      this.expertsBtn1.Size = new System.Drawing.Size(179, 38);
+      this.expertsBtn1.TabIndex = 3;
+      this.expertsBtn1.Text = "Tester";
+      this.expertsBtn1.TextColor = System.Drawing.Color.Black;
+      this.expertsBtn1.UseVisualStyleBackColor = false;
       // 
       // frmDoctors
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.AutoSize = true;
-      this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
       this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(41)))), ((int)(((byte)(90)))));
-      this.ClientSize = new System.Drawing.Size(800, 474);
+      this.ClientSize = new System.Drawing.Size(800, 530);
+      this.Controls.Add(this.panel3);
       this.Controls.Add(this.tabControl1);
-      this.Controls.Add(this.label1);
+      this.Controls.Add(this.panel1);
       this.Name = "frmDoctors";
       this.Text = "frmDoctors";
       this.Load += new System.EventHandler(this.frmDoctors_Load);
+      this.panel1.ResumeLayout(false);
+      this.panel1.PerformLayout();
       this.tabControl1.ResumeLayout(false);
+      this.panel3.ResumeLayout(false);
       this.ResumeLayout(false);
-      this.PerformLayout();
 
     }
 
     #endregion
 
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.Label lblPatientsNumbers;
     private System.Windows.Forms.Label label1;
     private System.Windows.Forms.TabControl tabControl1;
-    private System.Windows.Forms.TabPage tabPage1;
+    private System.Windows.Forms.TabPage tpListDoctors;
     private System.Windows.Forms.TabPage tabPage2;
+    private System.Windows.Forms.Panel panel3;
+    private FontAwesome.Sharp.IconButton iconButton1;
+    private ExpertsBtn expertsBtn1;
   }
 }
--- Dental App/Dental App/Doctors/frmDoctors.cs
+++ Dental App/Dental App/Doctors/frmDoctors.cs
@@ -22,6 +22,6 @@ namespace Dental_App.Doctors
 
     }
 
-  
+   
   }
 }
